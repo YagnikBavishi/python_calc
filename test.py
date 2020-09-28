7cce3a1c59b2eb9e1c80e9472398dc2c966ea929
YagnikBavishi <yagnikbavishi003@gmail.com>
2020-09-28 10:58:01 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,16 @@
+import sys
+from PyQt5.QWidget import QApplication
+from PyQt5.QWidget import QLabel
+from PyQt5.QWidget import QWidget
+app = QApplication(sys.argv)
+
+window = QWidget()
+window.setWindowTitle('PyQt5 App')
+window.setGeometry(100, 100, 280, 80)
+window.move(600, 15)
+helloMsg = QLabel('This is Test', parent=window)
+helloMsg.move(60, 30)
+
+window.show()
+
+sys.exit(app.exec_())
