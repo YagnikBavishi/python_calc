51394cf255959744216cbe88be6743dd09a036e7
YagnikBavishi <62586266+YagnikBavishi@users.noreply.github.com>
2020-09-28 14:29:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
-
-
+from  model import evaluateExpression
+from controller import Controller
 from PyQt5.QtWidgets import QApplication
 from view import GUI
 # Client code
@@ -8,13 +8,17 @@ def main():
     """Main function."""
     # Create an instance of QApplication
     pycalc = QApplication(sys.argv)
+
+
     # Show the calculator's GUI
     view = GUI()
     view.show()
 
+    model = evaluateExpression
+    Controller(model=model, view=view)
     sys.exit(pycalc.exec_())
 
 
-    
+
 if __name__ == '__main__':
     main()
