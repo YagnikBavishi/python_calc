6c8792314fa782e6d59d0654f13e2a3edb51b9e7
YagnikBavishi <62586266+YagnikBavishi@users.noreply.github.com>
2020-09-28 14:48:25 +0530

    Update controller.py

--- controller.py
+++ controller.py
@@ -1,21 +1,14 @@
 from functools import partial
-
 ERROR_MSG = 'ERROR'
-
 
 class Controller:
 
-    def _init_(self, model, view):
+    def __init__(self, model, view):
         """Controller initializer."""
         self._evaluate = model
-        self._view = view
+        self._view 	   = view
         # Connect signals and slots
         self._connectSignals()
-
-    def _calculateResult(self):
-        """Evaluate expressions."""
-        result = self._evaluate(expression=self._view.getDisplayText())
-        self._view.setDisplayText(result)
 
     def _buildExpression(self, sub_exp):
         """Build expression."""
@@ -25,6 +18,13 @@ class Controller:
         expression = self._view.getDisplayText() + sub_exp
         self._view.setDisplayText(expression)
 
+    def _buildExpression(self, sub_exp):
+        """Build expression."""
+        if self._view.getDisplayText() == ERROR_MSG:
+            self._view.clearDisplay()
+
+        expression = self._view.getDisplayText() + sub_exp
+        self._view.setDisplayText(expression)
     def _connectSignals(self):
         """Connect signals and slots."""
         for btnText, btn in self._view.buttons.items():
@@ -34,3 +34,8 @@ class Controller:
         self._view.buttons['='].clicked.connect(self._calculateResult)
         self._view.display.returnPressed.connect(self._calculateResult)
         self._view.buttons['C'].clicked.connect(self._view.clearDisplay)
+
+    def _calculateResult(self):
+        """Evaluate expressions."""
+        result = self._evaluate(expression=self._view.getDisplayText())
+        self._view.setDisplayText(result)
